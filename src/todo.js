652bd5f3c0e89373415e5702a87dbec6c1158495
Gianluca Miglietta <163427589+giallumigliet@users.noreply.github.com>
2026-09-10 09:49:37 +0200

    Update todo.js

--- src/todo.js
+++ src/todo.js
@@ -43,7 +43,7 @@ const TODO_CATEGORIES = [
   { id: "work", label: "Work", icon: "💼" },
   { id: "freeTime", label: "Free time", icon: "🎨" },
   { id: "car", label: "Car", icon: "🚗" },
-  { id: "travel", label: "Travel", icon: "🎁" },
+  { id: "travel", label: "Travel", icon: "✈️" },
   { id: "health", label: "Health", icon: "❤️" },
   { id: "study", label: "Study", icon: "📚" },
   { id: "pets", label: "Pets", icon: "🐾" },
